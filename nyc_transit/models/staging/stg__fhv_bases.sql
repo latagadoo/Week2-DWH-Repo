33965a73821b8de58e4edc4ecf7c075e991c8371
Lata Gadoo <lata_gadoo@hotmail.com>
2023-07-17 10:03:50 -0500

    Updated sql

--- nyc_transit/models/staging/stg__fhv_bases.sql
+++ nyc_transit/models/staging/stg__fhv_bases.sql
@@ -1,18 +1,15 @@
- with source as (
-	select * from {{ source('main', 'fhv_bases') }}
- ),
+ WITH source AS (
+    SELECT * FROM {{ source('main', 'fhv_bases') }}
+),
+renamed AS (
+    SELECT
+        TRIM(UPPER(COALESCE(base_number, ''))) AS base_number,
+        base_name,
+        dba,
+        dba_category,
+        filename
+    FROM source
+)
+SELECT *
+FROM renamed
 
- renamed as (
-	
-	select
-	   base_number,
-	   base_name,
-           dba,
-           dba_category,
-           filename
-
-	from source
-
- )
-
- select * from renamed
