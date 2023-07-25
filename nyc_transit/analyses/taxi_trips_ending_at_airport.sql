14415f85e0b88de6ac4c64a30a1d5204d81fc417
Lata Gadoo <lata_gadoo@hotmail.com>
2023-07-25 17:23:16 -0500

    Updated from solutions

--- nyc_transit/analyses/taxi_trips_ending_at_airport.sql
+++ nyc_transit/analyses/taxi_trips_ending_at_airport.sql
@@ -1,11 +1,8 @@
-SELECT
-    service_zone,
-    COUNT(*) AS total_trips,
-FROM {{ ref('mart__fact_all_taxi_trips') }} taxitrips
-JOIN {{ ref('mart__dim_locations') }} locations ON taxitrips.dolocationid = locations.locationid
-WHERE locations.service_zone IN ('Airports', 'EWR')
-GROUP BY locations.service_zone
-
+select count(*) as trips
+from {{ ref('mart__fact_all_taxi_trips') }} t
+join {{ ref('mart__dim_locations') }} dl on t.DOlocationID = dl.LocationID
+where dl.service_zone in ('Airports', 'EWR')
+group by all
 -- total number of trips ending in service_zones 'Airports' or 'EWR'	
 
 
